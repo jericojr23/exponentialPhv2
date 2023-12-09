5e51854c88d6c56cc907242fa1492f2b9884ff4c
Kurt <morroccollo@gmail.com>
2023-12-09 17:07:38 +0800

    scroll

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+
+
+module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    // Add the following line to enable the legacy behavior
+    // For Next.js 12 and later, this is not necessary.
+    // target: 'serverless',
+  };
+  
